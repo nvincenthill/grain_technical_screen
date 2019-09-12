a2ce7b274c040f371a23bce7ae00c6a88107bbb8
mypersonalgithubaccount <nvincenthill@gmail.com>
2019-09-12 14:40:48 -0700

    Configure boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const StyledApp = styled.div`
+  text-align: center;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+`;
+
+const Header = styled.header`
+  background-color: #0f1343;
+  min-height: 20vh;
+  flex-direction: column;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 400;
+  font-size: 1rem;
+  color: white;
+`;
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hello: 'world'
+    };
+  }
+
+  render() {
+    return (
+      <StyledApp>
+        <Header>
+          <h1>Technical Screen Boilerplate</h1>
+        </Header>
+      </StyledApp>
+    );
+  }
 }
 
 export default App;
